140cbaa93296ab2f00a836b20466b0d42ca10931
Ellen Körbes <ellenkorbes@gmail.com>
2019-01-28 21:53:22 +0100

    water

--- undo.sh
+++ undo.sh
@@ -3,5 +3,7 @@ echo "Commit hash:"
 read $hash
 git reset --hard $hash
 git push --force
+git tag -d 1.1.0-sparkling
+git tag -d 1.2.0-hot
 git push --delete origin 1.1.0-sparkling
 git push --delete origin 1.2.0-hot
